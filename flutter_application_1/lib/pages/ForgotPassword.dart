00bb2aae328b9ff584560a3ffbf399f5b92c6fe6
Ananda20062002 <Ananda21062002@gmail.com>
2022-04-04 17:29:25 +0700

    Register dan Forgot Password

---
+++ flutter_application_1/lib/pages/ForgotPassword.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({ Key? key }) : super(key: key);
+
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset('assets/logowhite.png',width: 54,height: 38,),
+        backgroundColor: Color(0xff50777A),
+        centerTitle: true,
+      ),
+
+      body: Container(
+        width: 1000,
+        decoration: BoxDecoration(color: Color(0xffE7DFD4)),
+        child : Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 50),
+            child :Image.asset('assets/logogreen.png',width: 200,height: 130,)
+            ),
+
+            Container(
+              margin: EdgeInsets.only(top: 50),
+              width: 320,
+              decoration: BoxDecoration(color : Color.fromARGB(40, 32, 62, 88),borderRadius: BorderRadius.all(Radius.circular(100)),),
+              child :TextFormField(
+                style: TextStyle(fontSize: 15,color:Color.fromRGBO(255, 255, 255, 60,) ),
+            decoration: InputDecoration(
+              hintText: 'Enter Your email address',
+              hintStyle: TextStyle(color: Color.fromRGBO(255, 255, 255, 60,)),
+              prefixIcon: Icon(Icons.email_outlined,color: Color.fromRGBO(255, 255, 255, 60,),),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(100))
+              ),)
+            ),
+            ),
+            Container(
+
+              margin: EdgeInsets.only(left: 35, right: 35,top: 10),
+              child: const Text("We will Send you a message to set or reset our new passsword",style: TextStyle(color: Color.fromRGBO(115, 114, 114, 0.6)),),
+            ),
+            Container( 
+              margin: EdgeInsets.only(top: 20),
+              child: ElevatedButton(onPressed: (){}, child: Text("Send Now",style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1,),fontWeight: FontWeight.bold),),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal : 50),
+                primary: Color.fromRGBO(32, 62, 88, 1),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(100))
+                )
+              ),),
+            ),
+            ],
+      ),
+      ),
+    );
+  }
+}
--- flutter_application_1/lib/pages/register.dart
+++ flutter_application_1/lib/pages/register.dart
@@ -1,18 +1,14 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_application_1/pages/ForgotPassword.dart';
-import 'package:flutter_application_1/pages/register.dart';
+import 'package:flutter_application_1/pages/login.dart';
 
-class login extends StatefulWidget {
-  const login({ Key? key }) : super(key: key);
+class Register extends StatefulWidget {
+  const Register({ Key? key }) : super(key: key);
 
   @override
-  State<login> createState() => _loginState();
+  State<Register> createState() => _RegisterState();
 }
 
-class _loginState extends State<login> {
+class _RegisterState extends State<Register> {
   bool visibilty = true;
   @override
   Widget build(BuildContext context) {
@@ -22,18 +18,19 @@ class _loginState extends State<login> {
         decoration: BoxDecoration(color: Color(0xffE7DFD4)),
         child : Column(
         children: [
+          
           Container(
             margin: EdgeInsets.only(top: 50),
-            child :Image.asset('assets/logogreen.png',width: 296,height: 200,)
+            child :Image.asset('assets/logogreen.png',width: 140,height: 97,)
             ),
             Container(
-              margin: EdgeInsets.only(top: 30),
+              margin: EdgeInsets.only(top: 10),
               width: 320,
               decoration: BoxDecoration(color : Color.fromARGB(40, 32, 62, 88),borderRadius: BorderRadius.all(Radius.circular(100)),),
               child :TextFormField(
                 style: TextStyle(fontSize: 15,color:Color.fromRGBO(255, 255, 255, 60,) ),
             decoration: InputDecoration(
-              hintText: 'Enter Your Username',
+              hintText: 'Username',
               hintStyle: TextStyle(color: Color.fromRGBO(255, 255, 255, 60,)),
               prefixIcon: Icon(Icons.person_outline_outlined,color: Color.fromRGBO(255, 255, 255, 60,),),
               border: OutlineInputBorder(
@@ -42,7 +39,37 @@ class _loginState extends State<login> {
             ),
             ),
             Container(
-              margin: EdgeInsets.only(top: 10),
+              margin: EdgeInsets.only(top: 18),
+              width: 320,
+              decoration: BoxDecoration(color : Color.fromARGB(40, 32, 62, 88),borderRadius: BorderRadius.all(Radius.circular(100)),),
+              child :TextFormField(
+                style: TextStyle(fontSize: 15,color:Color.fromRGBO(255, 255, 255, 60,) ),
+            decoration: InputDecoration(
+              hintText: 'Email Address',
+              hintStyle: TextStyle(color: Color.fromRGBO(255, 255, 255, 60,)),
+              prefixIcon: Icon(Icons.email_outlined,color: Color.fromRGBO(255, 255, 255, 60,),),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(100))
+              ),)
+            ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 18),
+              width: 320,
+              decoration: BoxDecoration(color : Color.fromARGB(40, 32, 62, 88),borderRadius: BorderRadius.all(Radius.circular(100)),),
+              child :TextFormField(
+                style: TextStyle(fontSize: 15,color:Color.fromRGBO(255, 255, 255, 60,) ),
+            decoration: InputDecoration(
+              hintText: 'Mobile Phone',
+              hintStyle: TextStyle(color: Color.fromRGBO(255, 255, 255, 60,)),
+              prefixIcon: Icon(Icons.call_outlined,color: Color.fromRGBO(255, 255, 255, 60,),),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(100))
+              ),)
+            ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 18),
               width: 320,
               decoration: BoxDecoration(color : Color.fromARGB(40, 32, 62, 88),borderRadius: BorderRadius.all(Radius.circular(100)),),
               child :TextFormField(
@@ -69,25 +96,10 @@ class _loginState extends State<login> {
               ),)
             ),
             ),
-            Container(
-              width: 300,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Text("Remember Me")
-                    ],
-                  ),
-                  TextButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=> ForgotPassword()));
-                  }, child: Text("Forgot Password ?",style: TextStyle(color: Color.fromRGBO(80, 119, 122, 1),fontWeight: FontWeight.bold ),))
-                ],
-              ),
-            ),
+
             Container( 
-              margin: EdgeInsets.only(top: 10),
-              child: ElevatedButton(onPressed: (){}, child: Text("LOGIN",style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1,),fontWeight: FontWeight.bold)),
+              margin: EdgeInsets.only(top: 20),
+              child: ElevatedButton(onPressed: (){}, child: Text("Register",style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1,),fontWeight: FontWeight.bold)),
               style: ElevatedButton.styleFrom(
                 padding: EdgeInsets.symmetric(horizontal : 100),
                 primary: Color.fromRGBO(32, 62, 88, 1),
@@ -100,18 +112,20 @@ class _loginState extends State<login> {
               child : Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                Text("Don't have an account yet ?"),
+                Text("Already Have an account ?"),
                 TextButton(onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=> const Register()));
-                }, child: const Text("Register",style: TextStyle(fontWeight: FontWeight.bold),),
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> const login()));
+                }, child: const Text("Login",style: TextStyle(fontWeight: FontWeight.bold),),
                 style: TextButton.styleFrom(
                   primary: Color.fromRGBO(32, 62, 88, 1)
                 ),)
               ],)
-            )
+            ),
+            
             ],
       ),
       )
     );
   }
+  
 }
